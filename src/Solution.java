f67cda64c753d1c172ff22ff940426a299ce1558
developerK <jy8532@ajou.ac.kr>
2024-12-14 06:56:35 +0900

    solution1 commit2

--- src/Solution.java
+++ src/Solution.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
         int[] diffList = Arrays.stream(diffs).sorted().toArray();
-        int answer = 1, presentindex = 0;
+        int answer = 1;
         long interval = 0;
         for(int diff : diffList) {
             interval = 0;
@@ -18,32 +18,18 @@ public class Solution {
                 answer = diff;
                 break;
             }
-            presentindex++;
         }
         if(answer == 1) return answer;
-        int shortAnswer = diffList[presentindex -1], biggerAnswer = diffList[presentindex] + 1;
-
-        while(true)
+        int leftshiftsum = 0;
+        for(int i = 0; i < diffs.length; i++)
         {
-            interval = 0;
-            answer = (biggerAnswer + shortAnswer)/2;
-            for (int i = 0; i < diffs.length; i++) {
-                if (answer >= diffs[i]) interval += times[i];
-                else
-                {
-                    interval += (diffs[i] - answer) * (times[i - 1] + times[i]) + times[i];
-                }
-                if(interval > limit) break;
-            }
-            if(interval > limit)
-            {
-                shortAnswer = answer;
-            }
-            else if(answer +1 == biggerAnswer) break;
-            else {
-                biggerAnswer = answer;
-            }
+            if(answer <= diffs[i]) leftshiftsum += times[i - 1] + times[i];
         }
-        return answer;
+        while(interval <= limit)
+        {
+            interval += leftshiftsum;
+            answer--;
+        }
+        return answer+1;
     }
 }
