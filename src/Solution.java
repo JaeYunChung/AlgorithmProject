b29665458d04bffd1418fc79eb3f68c1978ff201
developerK <jy8532@ajou.ac.kr>
2024-12-14 07:44:15 +0900

    solution1 final solution with binary_search

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,31 @@
-import java.util.Arrays;
-
 public class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int[] diffList = Arrays.stream(diffs).sorted().toArray();
-        int answer = 1;
+        int max = 1, min = 1;
         long interval = 0;
-        for(int diff : diffList) {
-            interval = 0;
-            for (int i = 0; i < diffs.length; i++) {
-                if (diff >= diffs[i]) interval += times[i];
-                else {
-                    interval += (diffs[i] - diff) * (times[i - 1] + times[i]) + times[i];
-                }
-                if(interval > limit) break;
-            }
-            if(interval <= limit) {
-                answer = diff;
-                break;
-            }
-        }
-        if(answer == 1) return answer;
-        int leftshiftsum = 0;
+        int answer = 1;
         for(int i = 0; i < diffs.length; i++)
         {
-            if(answer <= diffs[i]) leftshiftsum += times[i - 1] + times[i];
+            if(max < diffs[i]) max = diffs[i];
+        }answer = (min+max)/2;
+        while(true) {
+            interval = 0;
+            for (int i = 0; i < diffs.length; i++) {
+                if (answer >= diffs[i]) interval += times[i];
+                else {
+                    interval += (diffs[i] - answer) * (times[i - 1] + times[i]) + times[i];
+                }
+                if (interval > limit) break;
+            }
+            if (interval <= limit) {
+                if(answer == max) return answer;
+                max = answer;
+                answer = (min + answer)/2;
+            }
+            else {
+                if(answer + 1 == max) return answer + 1;
+                min = answer;
+                answer = (max + answer)/2;
+            }
         }
-        while(interval <= limit)
-        {
-            interval += leftshiftsum;
-            answer--;
-        }
-        return answer+1;
     }
 }
